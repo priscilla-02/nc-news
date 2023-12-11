b1c53fc1488e58fa2243114545e77f4a090dd1fd
priscilla-02 <136003704+priscilla-02@users.noreply.github.com>
2023-12-11 15:51:43 +0000

    feat list of all articles

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,26 @@
 import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { useState } from "react";
+import ArticleList from "./components/ArticleList";
+import Header from "./components/Header";
+import MenuBar from "./components/MenuBar";
 
 function App() {
-  return <div>News Project</div>;
+  const [articles, setArticles] = useState([]);
+  return (
+    <BrowserRouter>
+      <Header />
+      <MenuBar />
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <ArticleList articles={articles} setArticles={setArticles} />
+          }
+        />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
