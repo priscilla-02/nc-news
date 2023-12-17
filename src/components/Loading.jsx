6fbeff8180627204cecf3aa413468a371f29aac1
priscilla-02 <136003704+priscilla-02@users.noreply.github.com>
2023-12-17 20:38:50 +0000

    fix loading display

--- src/components/Loading.jsx
+++ src/components/Loading.jsx
@@ -4,10 +4,24 @@ import HourglassBottomTwoToneIcon from "@mui/icons-material/HourglassBottomTwoTo
 
 const Loading = () => {
   const { isDarkMode } = useContext(DarkModeContext);
+
   return (
-    <div className={`bg-mode ${isDarkMode ? "dark" : "light"}`}>
-      <HourglassBottomTwoToneIcon className="animate-pulse" />
-      <span className="text-xl py-50">Loading...</span>
+    <div
+      className={`bg-mode ${isDarkMode ? "dark" : "light"} min-h-[150px] pt-14`}
+    >
+      <HourglassBottomTwoToneIcon
+        className="animate-pulse"
+        style={{
+          color: isDarkMode ? "#fff" : "#0284C7",
+        }}
+      />
+      <span
+        className={`text-xl py-50 ${
+          isDarkMode ? "text-secondary" : "text-primary"
+        }`}
+      >
+        Loading...
+      </span>
     </div>
   );
 };
